431b7fdf82e6dd93e17ec9c447212cbba5614ab5
1dyer <941521358@qq.com>
2023-09-20 22:07:11 +0800

    Site updated: 2023-09-20 22:07:10

--- js/weather.js
+++ js/weather.js
@@ -16,8 +16,8 @@ WIDGET = {
       "fixed": "true",
       "vertical": "top",
       "horizontal": "left",
-      "left": "160",
-      "top": "5",
+      "left": "100",
+      "top": "0",
       "key": "8fb680da061248e4b6a79e0ea05230cc"
     }
   }
